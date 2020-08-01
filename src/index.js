2a1a68cc6a8a9632ea87f9b77719c48a58464333
M0D91 <msl.snzn@gmail.com>
2020-08-01 11:33:07 +0100

    agregado 'morgan': middleware para ver por consola las peticiones http que llegan al servidor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require ('express');
 const app = express();
 const port = 1414;
 
+const morgan = require('morgan');
+
+// middlewares
+app.use(morgan('dev'));
+
+
+// starting the server
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
 })
