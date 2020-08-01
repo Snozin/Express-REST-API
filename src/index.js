342bce84503872b4e0d6db5ec3ebc740beafa120
M0D91 <msl.snzn@gmail.com>
2020-08-01 12:19:10 +0100

    instalado 'nodemon' para poder ejecutar scripts automaticamente desde package.json. Modificado el apartado scripts de package.json para arrancar el server ejecutando npm run dev

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 const express = require ('express');
-const app = express();
-const port = 1414;
+const app = express()
 
 const morgan = require('morgan');
 
-// middlewares
-app.use(morgan('dev'));
 
+// settings
+app.set("port", process.env.PORT || 1414)
+
+// middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded( {extended:false} ))
+
+// routes
+app.get("/", (req, res) =>{
+    res.send("Hola people")
+})
 
 // starting the server
-app.listen(port, ()=>{
-    console.log(`Server listening on port ${port}`)
+app.listen(app.get("port"), ()=>{
+    console.log(`Server listening on port ${app.get("port")}`)
 })
+
+// app.get("/", (req, res) =>{
+//     res.send("Hola mundo")
+// })
+
+// app.listen(app.get("port"))
